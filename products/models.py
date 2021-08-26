af560fb94d425f376c5b14012211e1655c613363
bigmo <m.rezaie94@yahoo.com>
2021-08-26 12:06:23 +0430

    Add models to products/models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
+
+
+class Category(models.Model):
+    title = models.CharField(verbose_name=_('title'), max_length=50)
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = _('category')
+        verbose_name_plural = _('categories')
+
+
+class Product(models.Model):
+    name = models.CharField(verbose_name=_('name'), max_length=200)
+    color = models.CharField(verbose_name=_('color'), max_length=10)
+    price = models.IntegerField(verbose_name=_('price'))
+    seller = models.CharField(verbose_name=_('seller'), max_length=50)
+    category = models.ManyToManyField(Category)
+
+    class Meta:
+        db_table = 'product'
+        verbose_name = _('product')
+        verbose_name_plural = _('products')
