9788774fd5c2abec301c42bb6877c789157de44a
bigmo <m.rezaie94@yahoo.com>
2021-08-27 12:06:35 +0430

    Remove commands that use for debug

--- products/management/commands/crawl_products.py
+++ products/management/commands/crawl_products.py
@@ -1,6 +1,13 @@
 from django.core.management.base import BaseCommand
 from products.models import Category, Product
-from _crawler import get_categories
+from products.management.commands import _crawler
+
 
 class Command(BaseCommand):
-    pass
+    def handle(self, *args, **kwargs):
+        data = _crawler.crawl_product()
+        for item in data:
+            category = Category.objects.get_or_create(title=item[0])
+            if type(category) == tuple:
+                category = category[0]
+            Product.objects.create(category=category, name=item[1], price=item[2], color=item[3], seller=item[4])
