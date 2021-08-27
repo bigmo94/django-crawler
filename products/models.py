79f393d7cfeda2f56ebba40bd1eb956aa2f96a0b
bigmo <m.rezaie94@yahoo.com>
2021-08-27 12:24:42 +0430

    Add Created & Update time attributes to classes

--- products/models.py
+++ products/models.py
@@ -6,6 +6,9 @@ from django.utils.translation import gettext_lazy as _
 class Category(models.Model):
     title = models.CharField(verbose_name=_('title'), max_length=50)
 
+    created_at = models.DateTimeField(verbose_name=_('Created At'), auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name=_('Updated At'), auto_now=True)
+
     def __str__(self):
         return self.title
 
@@ -18,9 +21,12 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(verbose_name=_('name'), max_length=200)
     color = models.CharField(verbose_name=_('color'), max_length=10)
-    price = models.CharField(verbose_name=_('price'),max_length=50, default='')
+    price = models.CharField(verbose_name=_('price'), max_length=50, default='')
     seller = models.CharField(verbose_name=_('seller'), max_length=50)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(verbose_name=_('Created At'), auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name=_('Updated At'), auto_now=True)
 
     def __str__(self):
         return self.name
