aac675544816166aafb8a1d0ac67ff4faa32cb90
bigmo <m.rezaie94@yahoo.com>
2021-09-01 15:53:28 +0430

    Add is_enable field to list display

--- products/admin.py
+++ products/admin.py
@@ -4,13 +4,13 @@ from products.models import Category, Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['title', 'created_time', 'updated_time']
+    list_display = ['title', 'created_time', 'updated_time', 'is_enable']
     list_filter = ['title']
     search_fields = ['title']
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'price', 'color', 'category']
+    list_display = ['name', 'price', 'color', 'category', 'is_enable']
     list_filter = ['category']
     search_fields = ['name', 'category']
