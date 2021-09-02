6d3f1e4dd32731412607c910b418a119c9d4e42b
bigmo <m.rezaie94@yahoo.com>
2021-09-02 10:58:56 +0430

    Add MEDIA & STATIC ROOT to setting

--- django_crawler/settings.py
+++ django_crawler/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from decouple import config, Csv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,7 +92,11 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
