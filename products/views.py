5572b4fff6e750525b809c644fb919b0181c6176
bigmo <m.rezaie94@yahoo.com>
2021-09-02 10:16:59 +0430

    Add view to products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Category, Product
+
+
+def category_list(request):
+    category = Category.objects.all()
+    context = {'categories': category}
+    return render(request, "products/category_list.html", context)
+
+
+def product_list(request, category_id):
+    products = Product.objects.filter(category__id=category_id)
+    context = {'products': products}
+    return render(request, 'products/product_list.html', context)
+
+
+def product_detail(requset, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    context = {'product': product}
+    return render(requset, 'products/product_detail.html', context)
