8176ea299b4fdb08039707691fdf3157b8e4de79
bigmo <m.rezaie94@yahoo.com>
2021-09-04 12:03:20 +0430

    Add logout url and static file config

--- django_crawler/urls.py
+++ django_crawler/urls.py
@@ -1,8 +1,16 @@
-
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', include('products.urls'))
+    path('products/', include('products.urls')),
+    path('account/', include('users.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
