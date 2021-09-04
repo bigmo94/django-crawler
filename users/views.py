3ff01e8d20852c96fd6b0bc0efa7d377de84685c
bigmo <m.rezaie94@yahoo.com>
2021-09-04 12:05:32 +0430

    Create view for login & logout & profile detail

--- users/views.py
+++ users/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.contrib.auth import authenticate, login, logout
+from .forms import LoginForm
+from django.views import generic
+from .models import Profile
+
+
+class LoginUser(generic.View):
+    def get(self, *args, **kwargs):
+        form = LoginForm()
+        context = {'form': form}
+        return render(self.request, "users/login.html", context)
+
+    def post(self, *args, **kwargs):
+        form = LoginForm(self.request.POST)
+        if form.is_valid():
+            user = authenticate(**form.cleaned_data)
+            if user is not None:
+                login(self.request, user)
+                return redirect(f'/account/profile-detail/{user.id}/')
+            return self.get(self.request)
+
+
+class ProfileDetailView(generic.DetailView):
+    template_name = 'users/profile_detail.html'
+    model = Profile
+    context_object_name = 'profile'
+
+    def get_object(self):
+        return Profile.objects.get(user_id=self.kwargs['profile_id'])
+
+
+class LogoutUser(generic.View):
+    def get(self, request):
+        logout(request)
+        return render(request, 'users/logout.html')
