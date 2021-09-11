d72a71d0b2328624bbfb7eeb05fda4d7468d115a
bigmo <m.rezaie94@yahoo.com>
2021-09-11 11:12:19 +0430

    Add registration from

--- users/forms.py
+++ users/forms.py
@@ -4,3 +4,13 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput())
+
+
+class ProfileForm(forms.Form):
+    age = forms.IntegerField()
+    phone_number = forms.IntegerField()
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    email = forms.EmailField(required=False)
+    bio = forms.CharField(required=False)
+    avatar = forms.ImageField(required=False)
