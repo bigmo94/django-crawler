f4e0403dbbac9eee5b46e55130b87abd3a7840aa
bigmo <m.rezaie94@yahoo.com>
2021-09-11 11:37:09 +0430

    Config url setting

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
-from .views import category_list, product_list, product_detail
+
+from .views import (category_list,
+                    product_list,
+                    product_detail,
+                    CategoryListAPIView,
+                    ProductsAPIView,
+                    ProductDetailAPIView)
 
 app_name = 'products'
 
 urlpatterns = [
     path('category-list/', category_list, name='category_list'),
     path('product-list/<int:category_id>/', product_list, name="product_list"),
-    path('product-detail/<int:product_id>/', product_detail, name='product_detail')
+    path('product-detail/<int:product_id>/', product_detail, name='product_detail'),
+    path('category-api/', CategoryListAPIView.as_view(), name='category_api'),
+    path('product-api/', ProductsAPIView.as_view(), name='product-api'),
+    path('product-api/<int:id>/', ProductDetailAPIView.as_view(), name='products_detail_api'),
 ]
