8569d1358f06e2379746140f26ef969c3be8da96
bigmo <m.rezaie94@yahoo.com>
2021-09-11 11:36:24 +0430

    Create APIView for category and products

--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 
+from rest_framework import generics
+
+from .serializer import CategorySerializer, ProductSerializer
 from .models import Category, Product
 
 
@@ -19,3 +22,20 @@ def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     context = {'product': product}
     return render(request, 'products/product_detail.html', context)
+
+
+class CategoryListAPIView(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class ProductDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'id'
+
+
+class ProductsAPIView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
