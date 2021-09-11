91585fa401dc1b6676e77934c345876e59c31065
bigmo <m.rezaie94@yahoo.com>
2021-09-11 11:11:33 +0430

    Add registration view

--- users/views.py
+++ users/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render, redirect
 
 from django.contrib.auth import authenticate, login, logout
-from .forms import LoginForm
+from django.contrib.auth.forms import UserCreationForm
+from .forms import LoginForm, ProfileForm
 from django.views import generic
 from .models import Profile
+
+
+def registration(request):
+    form = UserCreationForm(request.POST or None)
+
+    if request.method == 'POST':
+        form2 = ProfileForm(data=request.POST, files=request.FILES)
+        if form.is_valid() and form2.is_valid():
+            user = form.save(commit=False)
+            first_name = form2.cleaned_data.get('first_name')
+            last_name = form2.cleaned_data.get('last_name')
+            email = form2.cleaned_data.get('email')
+            bio = form2.cleaned_data.get('bio')
+            avatar = form2.cleaned_data.get('avatar')
+            user.first_name = first_name
+            user.last_name = last_name
+            user.email = email
+            age = form2.cleaned_data.get('age')
+            phone_number = form2.cleaned_data.get('phone_number')
+            user.save()
+            Profile(user=user, age=age, phone_number=phone_number, avatar=avatar, bio=bio).save()
+            login(request, user)
+            return redirect('users:login')
+    form2 = ProfileForm()
+    context = {'form': form,
+               'form2': form2}
+    return render(request, 'users/register.html', context)
 
 
 class LoginUser(generic.View):
