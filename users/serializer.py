ac08165800dd3be870c66baabb8512fd887888d6
bigmo <m.rezaie94@yahoo.com>
2021-09-13 14:00:10 +0430

    Create serialiazer for register with email

--- users/serializer.py
+++ users/serializer.py
@@ -1,9 +1,17 @@
+import random
+
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from django.core.cache import cache
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from rest_framework.exceptions import ValidationError
 from .models import Profile
+from .utils import send_verification_code
+
+
+def generate_code():
+    return random.randint(10000, 99999)
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -48,3 +56,53 @@ class UserSerializer(serializers.ModelSerializer):
         if request.user == instance:
             data['token'] = instance.auth_token.key
         return data
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email',)
+
+    def create(self, validated_data):
+        email = validated_data['email']
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            user = User.objects.create_user(
+                username=validated_data.get('username'),
+                password=validated_data.get('password'),
+                first_name=validated_data.get('first_name', ''),
+                last_name=validated_data.get('last_name', ''),
+                email=validated_data.get('email'),
+            )
+
+        verify_code = generate_code()
+        cache_key = 'login_code_{}'.format(email)
+        cache.set(cache_key, verify_code, timeout=120)
+
+        send_verification_code(user.email, verify_code)
+
+        return user
+
+
+class ObtainTokenSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    token = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'token')
+
+    def create(self, validated_data):
+        user = authenticate(**validated_data)
+
+        if not user:
+            raise ValidationError('Wrong credentials sent')
+
+        Token.objects.get_or_create(user=user)
+
+        return user
+
+    def get_token(self, obj):
+        return obj.auth_token.key
